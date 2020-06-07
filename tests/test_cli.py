b9ca3679686f73f9e4d8aff14a450b67dafaa27c
gijsbertbas <gijs@connectholland.nl>
2020-06-07 14:34:14 +0200

    update tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,38 +1,33 @@
 import sys
 import pytest
 
-from segysak._cli import main, NAME
+from segysak._cli import cli, NAME
 
-from test_fixtures import temp_dir, temp_segy, TEMP_TEST_DATA_DIR
+from tests.test_fixtures import temp_dir, temp_segy, TEMP_TEST_DATA_DIR
 
 
 @pytest.mark.parametrize("help_arg", ["-h", "--help"])
 def test_help(help_arg):
     sys.argv = ["", help_arg]
     with pytest.raises(SystemExit):
-        main()
+        cli()
 
 
-def test_vertsion():
-    sys.argv = ["", "-V"]
+def test_version():
+    sys.argv = ["", "-v"]
     with pytest.raises(SystemExit):
-        main()
+        cli()
 
 
 def test_no_input_file():
     sys.argv = [""]
     with pytest.raises(SystemExit):
-        main()
+        cli()
 
 
-def test_logging_no_spec():
-    sys.argv = ["", "-L"]
-    with pytest.raises(SystemExit):
-        main()
-
-
-@pytest.mark.parametrize("ebc", ["-e", "--ebcidc"])
+@pytest.mark.parametrize("ebc", ["ebcidc"])
 def test_dump_ebcidc(temp_dir, temp_segy, ebc):
     # test_file = temp_dir / "ebcidc_dump.txt"
-    sys.argv = ["", str(temp_segy), ebc]
-    main()
+    sys.argv = ["", ebc, str(temp_segy)]
+    with pytest.raises(SystemExit):
+        cli()
