5b8e918ac2996d939d005de2162484f7082f9406
gijsbertbas <gijs@connectholland.nl>
2020-06-13 17:23:28 +0200

    fix converting to segy using ncdf2segy

--- segysak/segy/_segy_writer.py
+++ segysak/segy/_segy_writer.py
@@ -1,4 +1,3 @@
-
 import importlib
 import xarray as xr
 import segyio
@@ -42,8 +41,16 @@ def _bag_slices(ind, n=10):
         bag.append(slice(prev, i + 1, 1))
     return bag
 
+
 def ncdf2segy(
-    ncfile, segyfile, CMP=False, iline=189, xline=193, il_chunks=10, silent=False
+    ncfile,
+    segyfile,
+    CMP=False,
+    iline=189,
+    xline=193,
+    il_chunks=10,
+    dimension=None,
+    silent=False,
 ):
     """Convert etlpy siesnc format (NetCDF4) to SEGY.
 
@@ -55,14 +62,26 @@ def ncdf2segy(
         xline (int, optional): Crossline byte location. Defaults to 193.
         il_chunks (int, optional): The size of data to work on - if you have memory
             limitations. Defaults to 10.
+        dimension (str): Data dimension to output, defaults to 'twt' or 'depth' whichever is present
         silent (bool, optional): Turn off progress reporting. Defaults to False.
     """
 
-    with xr.open_dataset(ncfile, chunks={"i": il_chunks}) as seisnc:
-        ni, nj, nk = seisnc.dims["i"], seisnc.dims["j"], seisnc.dims["k"]
-        z0 = int(seisnc.vert.values[0])
-        msys = _ISEGY_MEASUREMENT_SYSTEM[seisnc.measurement_system]
+    with xr.open_dataset(ncfile, chunks={"iline": il_chunks}) as seisnc:
+        if dimension is None:
+            if seisnc.seis.is_twt():
+                dimension = "twt"
+            elif seisnc.seis.is_depth():
+                dimension = "depth"
+            else:
+                raise RuntimeError(
+                    f"twt and depth dimensions missing, please specify a dimension to convert: {seisnc.dims}"
+                )
+
+        z0 = int(seisnc[dimension].values[0])
+        ni, nj, nk = seisnc.dims["iline"], seisnc.dims["xline"], seisnc.dims[dimension]
+        msys = _ISEGY_MEASUREMENT_SYSTEM[seisnc.seis.get_measurement_system()]
         spec = segyio.spec()
+
         # to create a file from nothing, we need to tell segyio about the structure of
         # the file, i.e. its inline numbers, crossline numbers, etc. You can also add
         # more structural information, but offsets etc. have sensible defautls. This is
@@ -78,12 +97,15 @@ def ncdf2segy(
         xl_val = seisnc["xline"].values
         il_val = seisnc["iline"].values
 
+        coord_scalar = seisnc.coord_scalar
+        coord_scalar_mult = np.power(abs(coord_scalar), np.sign(coord_scalar) * -1)
+
         il_bags = _bag_slices(seisnc["iline"].values, n=il_chunks)
 
         with segyio.create(segyfile, spec) as segyf:
             for ilb in tqdm(il_bags, desc="WRITING CHUNK", disable=silent):
                 ilbl = range(ilb.start, ilb.stop, ilb.step)
-                data = seisnc.isel(i=ilbl)
+                data = seisnc.isel(iline=ilbl)
                 for i, il in enumerate(ilbl):
                     il0, iln = il * nj, (il + 1) * nj
                     segyf.header[il0:iln] = [
@@ -91,19 +113,19 @@ def ncdf2segy(
                             segyio.su.offset: 1,
                             iline: il_val[il],
                             xline: xln,
-                            segyio.su.cdpx: cdpx,
-                            segyio.su.cdpy: cdpy,
+                            segyio.su.cdpx: int(cdpx * coord_scalar_mult),
+                            segyio.su.cdpy: int(cdpy * coord_scalar_mult),
                             segyio.su.ns: nk,
                             segyio.su.delrt: z0,
                         }
                         for xln, cdpx, cdpy in zip(
-                            xl_val, data.CDP_X.values[i, :], data.CDP_Y.values[i, :]
+                            xl_val, data.cdp_x.values[i, :], data.cdp_y.values[i, :]
                         )
                     ]
                     segyf.trace[il * nj : (il + 1) * nj] = data.data[i, :, :].values
             segyf.bin.update(
                 tsort=segyio.TraceSortingFormat.INLINE_SORTING,
-                hdt=int(seisnc.ds * 1000),
+                hdt=int(seisnc.sample_rate * 1000),
                 hns=nk,
                 mfeet=msys,
                 jobid=1,
