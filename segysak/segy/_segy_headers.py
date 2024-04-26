dc5a86a7f1b991c2339959caf905a24a8eb2ef47
trhallam <8454087+trhallam@users.noreply.github.com>
2024-04-26 15:08:47 +0100

    simplify head_as_dimensions

--- segysak/segy/_segy_headers.py
+++ segysak/segy/_segy_headers.py
@@ -131,42 +131,28 @@ def segy_header_scrape(
     return head_df
 
 
-def header_as_dimensions(head_df: pd.DataFrame, **dim_kwargs) -> Dict[str, np.array]:
+def header_as_dimensions(head_df: pd.DataFrame, dims: tuple) -> Dict[str, np.array]:
     """Convert dim_kwargs to a diction of dimensions. Also useful for checking
     geometry is correct and unique for each trace in a segy file header.
 
     Args:
-        head_df: The header Dataframe from `segy_header_scrape`.
-        dim_kwargs: Dimension names (str) and byte location (int) pairs. Byte
-            locations must be valid segy byte locations for the trace headers.
+        head_df: The header DataFrame from `segy_header_scrape`.
+        dims: Dimension names (str) as per head_df.
     """
-    # creating dimensions and new dataset
-    dims = dict()
-    dim_index_names = list()
-    dim_fields = list()
-
-    check_tracefield(list(dim_kwargs))
-
-    for dim in dim_kwargs:
-        # check the dimension byte location to get the head_df column name
-        trace_field = str(segyio.TraceField(dim_kwargs[dim]))
-        dim_fields.append(trace_field)
-
+    unique_dims = dict()
+    for dim in dims:
         # get unique values of dimension and sort them ascending
-        as_unique = head_df[trace_field].unique()
-        dims[dim] = np.sort(as_unique)
+        as_unique = head_df[dim].unique()
+        unique_dims[dim] = np.sort(as_unique)
 
-    if (
-        head_df[dim_index_names].shape
-        != head_df[dim_index_names].drop_duplicates().shape
-    ):
+    if head_df[dims].shape != head_df[dims].drop_duplicates().shape:
         raise ValueError(
             "The selected dimensions results in multiple traces per "
             "dimension location, add additional dimensions or use "
             "trace numbering byte location to load as 2D."
         )
 
-    return dims
+    return unique_dims
 
 
 def what_geometry_am_i(head_df: pd.DataFrame):
