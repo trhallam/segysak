fa330b86aaa3bb0585e7ab349efe9b804582bc34
trhallam <8454087+trhallam@users.noreply.github.com>
2024-04-30 12:03:00 +0100

    dataclass fixes for Python3.9

--- segysak/_core.py
+++ segysak/_core.py
@@ -1,22 +1,25 @@
 """ Functions needed elsewhere in the library.
 """
 
+from typing import Tuple, Generator, Any, Dict
+
 
 class MetaDataClass:
 
-    def __iter__(self):
-        for match in self.__match_args__:
-            yield match
+    def __iter__(self) -> Generator[str, None, None]:
+        return self.keys()
 
-    def keys(self):
-        return self.__match_args__
+    def keys(self) -> Generator[str, None, None]:
+        for field in self.__dataclass_fields__:
+            yield field
 
-    def values(self):
-        for match in self.__match_args__:
-            yield self.__getattr__(match)
+    def values(self) -> Generator[Any, None, None]:
+        for field in self.keys():
+            yield self.__getattr__(field)
 
-    def items(self):
-        return {key: value for key, value in zip(self.keys(), self.values())}
+    def items(self) -> Generator[Tuple[str, Any], None, None]:
+        for key in self.keys():
+            yield key, self.__getitem__(key)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: str) -> Any:
         return self.__dict__[item]
