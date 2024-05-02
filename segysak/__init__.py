ce5c29d63b28ec799174cc421db7480770f470f5
trhallam <8454087+trhallam@users.noreply.github.com>
2024-05-02 15:12:10 +0100

    remove non page related stuff

--- segysak/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._version import version as __version__
-from ._accessor import SeisIO, SeisGeom, open_seisnc, coordinate_df
-
-from ._seismic_dataset import (
-    create_seismic_dataset,
-    create3d_dataset,
-    create2d_dataset,
-)
