add get_coords and fix scaling method

segysak/_accessor.py
@@ -345,13 +345,6 @@ class TemplateAccessor:
                 unknown_dims.append(dim)
         return inferred_dims
 
-    def is_3d(self):
-        dims = self.get_dimensions()
-        if DimKeyField.iline in dims and DimensionKeyField.xline in dims:
-            return True
-        else:
-            return False
-
 
 @xr.register_dataarray_accessor("segysak")
 class SegysakDataArrayAccessor(TemplateAccessor):
@@ -379,16 +372,31 @@ class SegysakDatasetAccessor(TemplateAccessor):
 
         self.store_attributes(**seisnc_coords)
 
+    def get_coords(self):
+        """Returns attrs['cdp_x'], attrs['cdp_y'] if exist else user infer_coords()"""
+        cdp_x = self.attrs.get(CoordKeyField.cdp_x)
+        cdp_y = self.attrs.get(CoordKeyField.cdp_y)
+
+        if cdp_x is None and cdp_y is None:
+            try:
+                cdp_x, cdp_y = self.infer_coords()
+            except ValueError:
+                raise KeyError(
+                    "Could not find or infer coordinates. Use set_coords() to set map coordinate name mapping for 'cdp_x' and 'cdp_y'"
+                )
+
+        return cdp_x, cdp_y
+
     def infer_coords(self, ignore: List[str] = None):
         """ """
         inferred_coords = {}
         unknown_coords = []
         for coord in CoordKeyField:
-            if coord in self._obj.data_vars:
+            if coord in self._obj:
                 inferred_coords[coord] = coord
             else:
                 unknown_coords.append(coord)
-        self.set_coords(inferred_coords)
+        return inferred_coords
 
     def scale_coords(self, coord_scalar: float = None):
         """Scale the coordinates using a SEG-Y coord_scalar
@@ -416,6 +424,8 @@ class SegysakDatasetAccessor(TemplateAccessor):
         if coord_scalar is None:
             coord_scalar = self.attrs.get(AttrKeyField.coord_scalar, None)
 
+        cdp_x, cdp_y = self.get_coords()
+
         # if no specified coord_scalar then set to 1
         if coord_scalar is None:
             coord_scalar = 1.0
@@ -423,16 +433,8 @@ class SegysakDatasetAccessor(TemplateAccessor):
         coord_scalar = float(coord_scalar)
         coord_multiplier = np.power(abs(coord_scalar), np.sign(coord_scalar))
 
-        iline_var = self.get(CoordKeyField.cdp_x)
-        xline_var = self.get(CoordKeyField.cdp_y)
-
-        if iline_var and xline_var:
-            self._obj[iline_var] = self._obj[iline_var] * coord_multiplier
-            self._obj[xline_var] = self._obj[xline_var] * coord_multiplier
-        else:
-            raise ValueError(
-                "Specify coordinate dimensions first using ds.segysak.set_coords()"
-            )
+        self._obj[cdp_x] = self._obj[cdp_x] * coord_multiplier
+        self._obj[cdp_y] = self._obj[cdp_y] * coord_multiplier
 
         self.store_attributes(
             **{AttrKeyField.coord_scalar: coord_scalar, AttrKeyField.coord_scaled: True}

tests/test_accessor.py
@@ -70,6 +70,19 @@ def segy_datasets(request, tests_path):
     return ds
 
 
+@pytest.fixture()
+def segy_datasets_nocoords(segy_datasets):
+    return segy_datasets.drop_vars(["cdp_x", "cdp_y"])
+
+
+@pytest.fixture()
+def segy_datasets_altcoords(segy_datasets):
+    new_labels = {"cdp_x": "cdpx", "cdp_y": "cdpy"}
+    ds = segy_datasets.rename_vars(new_labels)
+    ds.segysak.set_coords(new_labels)
+    return ds
+
+
 def test_get_blank_segysak_attrs(xrd):
     attrs = xrd.segysak.attrs
     assert attrs == dict()
@@ -143,12 +156,41 @@ def test_segysak_get_dimensions(segy_datasets):
 
 def test_scale_coords(segy_datasets):
     ds = segy_datasets
-    with pytest.raises(ValueError):
-        ds.segysak.scale_coords()
-    # ds.segysak.set_coords({"cdp_x": "cdp_x", "cdp_y": "cdp_y"})
-    ds.segysak.infer_coords()
     ds.segysak.scale_coords()
     assert ds.segysak["coord_scaled"]
+
+
+def test_scale_altcoords(segy_datasets_altcoords):
+    ds = segy_datasets_altcoords
+    ds.segysak.scale_coords()
+    assert ds.segysak["coord_scaled"]
+
+
+def test_scale_nocoords(segy_datasets_nocoords):
+    ds = segy_datasets_nocoords
+    with pytest.raises(KeyError):
+        ds.segysak.scale_coords()
+
+
+def test_calc_corner_points_3d(segy_datasets):
+    ds = segy_datasets
+    cp = ds.segysak.calc_corner_points()
+    assert len(cp) == 5
+
+
+def test_calc_corner_points_2d(volve_2d_dataset):
+    cp = volve_2d_dataset.segysak.calc_corner_points()
+    assert len(cp) == 2
+    assert np.allclose(
+        np.array(cp),
+        np.array(
+            [
+                (436482.16, 6477774.5),
+                (434044.3, 6477777.0),
+            ]
+        ),
+        rtol=0.1,
+    )
 
 
 @pytest.mark.parametrize(